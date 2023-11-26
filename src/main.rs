5d0fb842f5822ee13957a7c5a976a3f75af1617e
yexiyue <yexiyue666@qq.com>
2023-11-26 20:22:15 +0800

    reflect: remove dialogue-macro

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,5 @@
-use dialogue_macro::Dialogue;
-use dialoguer::{theme::ColorfulTheme, Confirm};
-// use wasm_startup::init;
-
-#[derive(Dialogue)]
-struct User {
-    #[dialogue(default = true, prompt = "是否是管理员")]
-    admin: bool,
-    #[dialogue(prompt = "请输入地址", default = "四川省达州市")]
-    address: String,
-    #[dialogue(options=["张三", "李四", "王五"],default="李四")]
-    names: String,
-    #[dialogue(options=["张三", "李四", "王五"],default=["李四","王五"])]
-    names2: Vec<String>,
-    #[dialogue(
-        prompt = "请输入密码",
-        password = true,
-        confirmation = "请再次输入密码"
-    )]
-    password: String,
-}
+use wasm_startup::init;
 
 fn main() {
-    User::admin();
-    User::address();
-    User::names("请选择组长");
-    User::names2("请选择组长2");
-    User::password();
-    // let res: Vec<String> = dialoguer::Input::with_theme(&ColorfulTheme::default())
-    //     // .default(24)
-    //     .with_prompt("请输入地址")
-    //     .interact()
-    //     .unwrap();
+    init();
 }
-// fn main() {
-//     init();
-// }
